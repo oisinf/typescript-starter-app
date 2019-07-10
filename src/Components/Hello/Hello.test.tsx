e1a23bde6a8f1a7fdfeda8ec624cbbd6598b2fda
oisinf <oisinfeelyspoon@gmail.com>
2019-07-10 16:22:11 +0100

    Test not throw when enthusiasm = 0

--- src/Components/Hello/Hello.test.tsx
+++ src/Components/Hello/Hello.test.tsx
@@ -1,34 +1,28 @@
-import * as React from "react";
-import * as enzyme from "enzyme";
-import { ComponentHello } from "./Hello";
+import * as React from 'react'
+import * as enzyme from 'enzyme'
+import { ComponentHello } from './Hello'
 
-describe("Render correctly given correct inputs and throw error given incorrect inputs", () => {
-  test("renders correct input when no enthusiasm level given", () => {
-    const hello = enzyme.shallow(<ComponentHello name="Test Name" />);
-    expect(hello.find(`[data-test='greeting-div']`).text()).toEqual(
-      "Hello Test Name!"
-    );
-  });
+describe('Render correctly given correct inputs and throw error given incorrect inputs', () => {
+    test('renders correct input when no enthusiasm level given', () => {
+        const hello = enzyme.shallow(<ComponentHello name="Test Name" />)
+        expect(hello.find(`[data-test='greeting-div']`).text()).toEqual('Hello Test Name!')
+    })
 
-  test("Renders the correct text with an explicting enthusiasm of 1", () => {
-    const hello = enzyme.shallow(
-      <ComponentHello name="Test Name" enthusiasmLevel={1} />
-    );
-    expect(hello.find(`[data-test="greeting-div"]`).text()).toEqual(
-      "Hello Test Name!"
-    );
-  });
+    test('Renders the correct text with an explicting enthusiasm of 1', () => {
+        const hello = enzyme.shallow(<ComponentHello name="Test Name" enthusiasmLevel={1} />)
+        expect(hello.find(`[data-test="greeting-div"]`).text()).toEqual('Hello Test Name!')
+    })
 
-  //Test failing unsure why
-  test("Throws when enthusiasm is at 0 ", () => {
-    expect(() => {
-      enzyme.shallow(<ComponentHello name="Test Name" enthusiasmLevel={0} />);
-    }).toThrow();
-  });
+    //Test failing unsure why
+    test('Throws when enthusiasm is at 0 ', () => {
+        expect(() => {
+            enzyme.shallow(<ComponentHello name="Test Name" enthusiasmLevel={0} />)
+        }).toThrow()
+    })
 
-  test("Throws when enthusiasm is less then 0", () => {
-    expect(() => {
-      enzyme.shallow(<ComponentHello name="Test Name" enthusiasmLevel={-10} />);
-    }).toThrow();
-  });
-});
+    test('Throws when enthusiasm is less then 0', () => {
+        expect(() => {
+            enzyme.shallow(<ComponentHello name="Test Name" enthusiasmLevel={-10} />)
+        }).toThrow()
+    })
+})
